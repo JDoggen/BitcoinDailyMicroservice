f48bd91c2aecfa67ad4985ba7deb8617b6fb37c4
jaapdew <dewijzejaap@gmail.com>
2018-09-24 19:09:09 +0200

    Folders gemaakt

--- BitcoinData/src/main/java/com/jjalgorithms/cryptocurrency/bitcoin/service/PredictionService.java
+++ BitcoinData/src/main/java/com/jjalgorithms/cryptocurrency/bitcoin/service/PredictionService.java
@@ -1,5 +1,15 @@
 package com.jjalgorithms.cryptocurrency.bitcoin.service;
 
+import java.util.List;
+
 public class PredictionService {
 
+	public Double calculateOneDayPrediction(List<Double> closeValues) {		//berekent op basis van 
+		double combinedCloses = 0.0;
+		for(Double close: closes) {										//1440min in een dag
+			combinedCloses += close;
+		}
+		double averageClose = combinedCloses/1440; 
+	
+	}
 }
