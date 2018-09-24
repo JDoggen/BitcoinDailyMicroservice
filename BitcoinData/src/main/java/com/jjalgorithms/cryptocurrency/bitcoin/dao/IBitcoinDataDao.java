d5bb0d59b856675244ed3c3e905ca569ca792649
JDoggen <jpdoggen@gmail.com>
2018-09-24 16:36:38 +0200

    BitcoinData finally added

---
+++ BitcoinData/src/main/java/com/jjalgorithms/cryptocurrency/bitcoin/dao/IBitcoinDataDao.java
@@ -0,0 +1,27 @@
+package com.jjalgorithms.cryptocurrency.bitcoin.dao;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jjalgorithms.cryptocurrency.bitcoin.model.BitcoinData;
+
+public interface IBitcoinDataDao extends CrudRepository<BitcoinData, Long>{
+	
+	@Override
+	public long count();
+	
+	@Override
+	public List<BitcoinData> findAll();	
+
+	@Override
+	public <S extends BitcoinData> S save(S s);
+	
+	@Override
+	public <S extends BitcoinData> List<S> saveAll(Iterable<S> s);
+	
+	List<BitcoinData> findByTimeStampBetween(Long timeStampStart, Long timeStampEnd);
+	
+	BitcoinData findFirstByOrderByTimeStamp();
+
+}
